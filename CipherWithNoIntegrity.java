f2b005fa658f1c425acad3ad9d8808b940da8f42
komalThombare1 <100417869+komalThombare1@users.noreply.github.com>
2022-04-20 22:24:02 +0530

    Update CipherWithNoIntegrity.java

--- CipherWithNoIntegrity.java
+++ CipherWithNoIntegrity.java
@@ -15,7 +15,7 @@ public class CipherWithNoIntegrity {
 	}
 	public void md2 () {
 		// EMB-ISSUE: CodeIssueNames.CIPHER_WITH_NO_INTEGRITY
-    Cipher c = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+    Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
 	c.init(Cipher.ENCRYPT_MODE, k, iv);
 	byte[] cipherText = c.doFinal(plainText);
 	}
@@ -33,7 +33,7 @@ public class CipherWithNoIntegrity {
 		}
 	public void md5 () {
 		// EMB-ISSUE: CodeIssueNames.CIPHER_WITH_NO_INTEGRITY/no-detect
-    Cipher c = Cipher.getInstance("DESede/GCM/PKCS5Padding");
+    Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
 	c.init(Cipher.ENCRYPT_MODE, k, iv);
 	byte[] cipherText = c.doFinal(plainText);
 	}
